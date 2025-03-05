4bfe727bc2fa09663ef3fc379de61252b6f84012
frank <no>
2025-03-05 16:12:33 +0800

    tidy

--- internal/packed/packed.go
+++ internal/packed/packed.go
@@ -2,6 +2,7 @@ package packed
 
 import (
 	"apis/internal/conf"
+	"apis/internal/logic/db"
 	"apis/internal/logic/ratelimit"
 	"apis/internal/logic/riskadmin"
 	"apis/internal/service"
@@ -9,5 +10,6 @@ import (
 
 func init() {
 	service.RegisterRiskAdmin(riskadmin.NewRiskAdmin())
+	service.RegisterDB(db.New())
 	service.RegisterRateLimiter(ratelimit.NewLimiter(conf.Config.Server.RateLimit))
 }
